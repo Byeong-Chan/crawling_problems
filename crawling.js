0237fe525e6b7184e8e6cdcabd6a72e37943671c
qudcksquf <SeoByeongChan@seobyeongchan-ui-MacBook-Pro.local>
2019-12-07 16:12:51 +0900

    BOJ 추가

--- crawling.js
+++ crawling.js
@@ -4,15 +4,16 @@ const config = require('./config.js');
 /* ---------
  mongoose connected */
 
-mongoose.connect(config.mongodbUri, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect(config.mongodbUri, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false });
 mongoose.Promise = global.Promise;
 
 const db = mongoose.connection;
 db.on('error', console.error);
 
 const model = require('./model');
-
 const Crawler = require('crawler');
+
+
 
 const prev = [];
 
@@ -22,6 +23,8 @@ const funcodeforces = async function(c) {
     c.queue(`https://codeforces.com/problemset/page/${idxc}`);
 };
 
+
+
 const prevs = [];
 
 let idxs = -1;
@@ -29,6 +32,33 @@ const funspoj = async function(s) {
     idxs += 1;
     s.queue(`https://www.spoj.com/problems/classical/sort=0,start=${idxs * 50}`);
 };
+
+
+
+
+let idxb_categroy = 0;
+const funboj_category = async function(b_category) {
+    idxb_categroy += 1;
+    b_category.queue(`https://solved.ac/problems/algorithms/${idxb_categroy}`);
+};
+
+
+
+
+let idxb = -1;
+const funboj = async function(b, b_category) {
+    idxb += 1;
+    if(idxb < 31) {
+        b.queue(`https://solved.ac/problems/level/${idxb}`);
+    }
+    else {
+        idxb_categroy = 0;
+        funboj_category(b_category);
+    }
+};
+
+
+
 
 const c = new Crawler({
     rateLimit: 1000,
@@ -50,42 +80,42 @@ const c = new Crawler({
             let cnt = 0;
             const arr = [];
             $(".problems td").each(function (idx) {
-                 $(this).find(".ProblemRating").each(function (idx) {
-                     if ($(this).text().trim() === '') return;
-                     // 문제 레이팅
-                     if ($(this).text().trim().length > 0) {
-                         obj.problem_rating = parseInt($(this).text().trim());
-                     }
-                 });
+                $(this).find(".ProblemRating").each(function (idx) {
+                    if ($(this).text().trim() === '') return;
+                    // 문제 레이팅
+                    if ($(this).text().trim().length > 0) {
+                        obj.problem_rating = parseInt($(this).text().trim());
+                    }
+                });
 
-                 $(this).find("a").each(function (idx) {
-                     if ($(this).text().trim() === '') return;
-                     if ($(this)[0].attribs.class === 'notice') {
-                         // 카테고리
-                         obj.Category.push($(this).text().trim());
-                     }
-                     else if ($(this)[0].attribs.title === 'Participants solved the problem') {
-                         // 푼 사람 수
-                         if($(this).text().trim().length > 0)
-                             obj.problem_solver = parseInt($(this).text().trim().substring(1));
-                     }
-                     else if ($(this)[0].attribs.title !== 'Participants solved the problem') {
-                         if(cnt === 0) {
-                             obj.problem_number = "codeforces/" + obj.problem_number;
-                             if(obj.name !== '') arr.push(obj);
-                             obj = {
-                                 name: '',
-                                 problem_number: '',
-                                 Category: [],
-                                 problem_solver: 0,
-                                 problem_rating: 0
-                             };
-                         }
-                         if(cnt === 0) obj.problem_number = $(this).text().trim();
-                         else obj.name = $(this).text().trim();
-                         cnt = (cnt + 1) % 2;
-                     }
-                 });
+                $(this).find("a").each(function (idx) {
+                    if ($(this).text().trim() === '') return;
+                    if ($(this)[0].attribs.class === 'notice') {
+                        // 카테고리
+                        obj.Category.push($(this).text().trim());
+                    }
+                    else if ($(this)[0].attribs.title === 'Participants solved the problem') {
+                        // 푼 사람 수
+                        if($(this).text().trim().length > 0)
+                            obj.problem_solver = parseInt($(this).text().trim().substring(1));
+                    }
+                    else if ($(this)[0].attribs.title !== 'Participants solved the problem') {
+                        if(cnt === 0) {
+                            obj.problem_number = "codeforces/" + obj.problem_number;
+                            if(obj.name !== '') arr.push(obj);
+                            obj = {
+                                name: '',
+                                problem_number: '',
+                                Category: [],
+                                problem_solver: 0,
+                                problem_rating: 0
+                            };
+                        }
+                        if(cnt === 0) obj.problem_number = $(this).text().trim();
+                        else obj.name = $(this).text().trim();
+                        cnt = (cnt + 1) % 2;
+                    }
+                });
             });
             if(obj.name !== '') arr.push(obj);
 
@@ -233,8 +263,139 @@ const s = new Crawler({
     }
 });
 
+let counter = 0;
+const b_category = new Crawler({
+    rateLimit: 1000,
+
+    // This will be called for each crawled page
+    callback: function (error, res, done) {
+        if (error) {
+            console.log(error);
+        } else {
+            const $ = res.$;
+            const category_name = $('h1').text().trim();
+
+            if(category_name === '' && counter < 10) {
+                counter += 1;
+                console.log('empty');
+                funboj_category(b_category).catch(err => {
+                    console.log(err)
+                });
+            }
+            if(category_name !== '') {
+                counter = 0;
+                console.log('BOJ Category ' + idxb_categroy + ' ' + category_name);
+
+                const arr = [];
+                $('.problem_list tr').each(function(idx) {
+                    const obj = {
+                        name: '',
+                        problem_number: '',
+                        problem_solver: 0,
+                        problem_rating: 0,
+                        Category: []
+                    };
+                    const keys = ["problem_number", "name", "problem_solver"];
+                    $(this).find('td').each(function(idx) {
+                        obj[keys[idx]] = $(this).text().trim();
+                        if(idx === 2) {
+                            const tmp = $(this).text().trim();
+                            if(tmp[tmp.length - 1] === 'K') {
+                                obj[keys[idx]] = parseInt(parseFloat(tmp.substring(0, tmp.length - 1)) * 1000);
+                            }
+                            else {
+                                let s = '';
+                                tmp.split(',').map((val, idx) => {
+                                    s = s.concat(val);
+                                });
+                                obj[keys[idx]] = parseInt(s);
+                            }
+                        }
+                    });
+                    if(obj.name !== '') {
+                        obj.problem_number = 'BOJ/' + obj.problem_number;
+                        arr.push(obj);
+                    }
+                });
+
+                for(let i = 0; i < arr.length; i++) {
+                    model.outProblem.create(arr[i]).then(result => {}).catch(err => {});
+                    model.outProblem.findOneAndUpdate({problem_number: arr[i].problem_number},
+                        {$addToSet: {Category: category_name}}).then(result => {}).catch(err => {});
+                }
+
+                funboj_category(b_category).catch(err => {
+                    console.log(err)
+                });
+            }
+        }
+        done();
+    }
+});
+
+const b = new Crawler({
+    rateLimit: 1000,
+
+    // This will be called for each crawled page
+    callback: function (error, res, done) {
+        if (error) {
+            console.log(error);
+        } else {
+            const $ = res.$;
+            console.log('BOJ ' + idxb);
+            const arr = [];
+            $('.problem_list tr').each(function(idx) {
+                const obj = {
+                    name: '',
+                    problem_number: '',
+                    problem_solver: 0,
+                    problem_rating: idxb,
+                    Category: []
+                };
+                const keys = ["problem_number", "name", "problem_solver"];
+                $(this).find('td').each(function(idx) {
+                    obj[keys[idx]] = $(this).text().trim();
+                    if(idx === 2) {
+                        const tmp = $(this).text().trim();
+                        if(tmp[tmp.length - 1] === 'K') {
+                            obj[keys[idx]] = parseInt(parseFloat(tmp.substring(0, tmp.length - 1)) * 1000);
+                        }
+                        else {
+                            let s = '';
+                            tmp.split(',').map((val, idx) => {
+                                s = s.concat(val);
+                            });
+                            obj[keys[idx]] = parseInt(s);
+                        }
+                    }
+                });
+                if(obj.name !== '') {
+                    obj.problem_number = 'BOJ/' + obj.problem_number;
+                    arr.push(obj);
+                }
+            });
+
+            for(let i = 0; i < arr.length; i++) {
+                model.outProblem.create(arr[i]).then(result => {}).catch(err => {});
+                model.outProblem.findOneAndUpdate({problem_number: arr[i].problem_number},
+                    {$set: {problem_rating: arr[i].problem_rating,
+                            problem_solver: arr[i].problem_solver}}).then(result => {}).catch(err => {});
+            }
+
+            funboj(b, b_category).catch(err => {console.log(err)});
+        }
+        done();
+    }
+});
+
 
 db.once('open', () => {
+    idxb = -1;
+    counter = 0;
+    funboj(b).catch(err => {
+        console.log(err);
+    });
+
     setInterval(() => {
         idxs = -1;
         funspoj(s).catch(err => {
